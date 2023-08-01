6b51687552a02c9c72980206a5bf72e324ccdd46
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-08-01 18:52:07 +0900

    [Finish]Section3

--- section3/main.js
+++ section3/main.js
@@ -5,3 +5,10 @@ greetings = 'Good Morning';
 greetings = 'Good Morning';
 greetings = 'Good Morning';
 greetings = 'Good Morning';
+
+function add(num1, num2) {
+  console.log(num1, num2);
+  const value = num1 + num2;
+  return value;
+}
+const returnedValue = add(2, 5);
