87a0a6ec6b0e49c5973af8bda5756732a677fc21
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-08-30 09:13:35 +0900

    [Edit]File Name & [FInish]Part160

---
+++ section10:配列をもっと使いこなす/main.js
@@ -0,0 +1,32 @@
+let fruits = ['apple', 'lemon'];
+fruits[2] = 'grape';
+fruits['hello'] = 'hello';
+fruits.length = 2;
+fruits = ['apple', , 'orange', 'grape'];
+fruits = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(fruits[(1)[2]]);
+
+// new Arrayを使った配列の作り方
+fruits = new Array('apple', 'orange', 'grape');
+fruits = new Array(1, 2, 3);
+fruits = new Array(1); // 数字が一つだけだとlengthだけが指定された配列になる(中身はempty)
+
+// newは省略できる
+fruits = Array('apple', 'orange', 'grape');
+fruits = Array(1, 2, 3);
+fruits = Array(1);
+
+// staticメソッドを使える
+fruits = Array.of('apple', 'orange', 'grape');
+fruits = Array.of(1, 2, 3);
+fruits = Array.of(1); //lengthしてにならず配列にちゃんとなる
+
+// スプレッド構文を使って展開する方法
+fruits = ['apple', 'orange', 'grape'];
+const newFruits = [...fruits];
+fruits.push('banana');
+console.log(newFruits, fruits);
